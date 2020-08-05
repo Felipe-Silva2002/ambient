00831656e52c8373fc5185397784d9b34a402272
FELIPE SILVA <felipe.silva@gsw.com.br>
2020-08-05 10:45:01 -0300

    first commit

---
+++ src/main/java/com/poc/ambient/AmbientApplication.java
@@ -0,0 +1,13 @@
+package com.poc.ambient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbientApplication.class, args);
+	}
+
+}
